71d0333edb9b06cd8ef4382b5c04d66be5efef80
Inesa Kostova <kostovainesa@gmail.com>
2025-02-21 15:06:22 +0200

    Add files via upload

---
+++ On time for the exam.cs
@@ -0,0 +1,46 @@
+﻿int startHour = int.Parse(Console.ReadLine());
+int startMin = int.Parse(Console.ReadLine());
+int arriveHour=int.Parse(Console.ReadLine());
+int arriveMin = int.Parse(Console.ReadLine());
+
+int startTime = startHour * 60 + startMin;
+int arriveTime = arriveHour * 60 + arriveMin;
+int differenceTime = Math.Abs(startTime - arriveTime);
+int differenceHour = differenceTime / 60;
+int differenceMin = differenceTime % 60;
+
+
+if(startTime< arriveTime)
+{
+    Console.WriteLine("Late");
+    if(differenceHour==0)
+    {
+        Console.WriteLine($"{differenceMin} minutes after the start");
+    }
+    else
+    {
+        Console.WriteLine($"{differenceHour}:{differenceMin:D2} hours after the start");
+    }
+}
+else if(differenceTime<=30)
+{
+    Console.WriteLine("On time");
+    if(differenceMin!=0)
+    {
+        Console.WriteLine($"{differenceMin} minutes before the start");
+
+    }
+}
+else
+{
+    Console.WriteLine("Early");
+    if (differenceHour == 0)
+    {
+        Console.WriteLine($"{differenceMin} minutes before the start");
+    }
+    else
+    {
+        Console.WriteLine($"{differenceHour}:{differenceMin:D2} hours before the start");
+    }
+
+}
