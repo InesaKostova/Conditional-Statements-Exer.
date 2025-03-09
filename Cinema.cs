dab750d7245fc2d39aa115bfa7943848dbc52f3f
Inesa Kostova <kostovainesa@gmail.com>
2025-03-09 08:52:01 +0200

    Cinema.cs

--- Cinema.cs
+++ Cinema.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string type = Console.ReadLine();
+int rows = int.Parse(Console.ReadLine());
+int colums = int.Parse(Console.ReadLine());
+double sum = 0;
+
+if (type =="Premiere")
+{
+    sum = rows * colums * 12;
+}
+else if (type =="Normal")
+{
+    sum = rows * colums * 7.50;
+}
+else
+{
+    sum = rows * colums * 5.00;
+}
+Console.WriteLine($"{sum:F2} leva");
